f97a629ff6b74ab33dcb62427d4dbf18cba19c00
John Edwin Floyd <johnedwinfloyd@Johns-MacBook.local>
2017-12-11 08:26:51 -0700

    working on functions

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
 
-$(document).keyup(function(event) {
+$(document).keyup(function(event) {//these two function are the same. One for racer1 key 29 and racer2 key 32.
 	console.log('hi3');
 	if (event.which == 39) {
 		$('#racer1').css('margin-left', '+=5%');
 	}
 
 
-});// this is hte end of the keyup for 39 function
+});//end of the keyup for 39 function 
 
 	
 
@@ -16,17 +16,28 @@ $(document).keyup(function(event) {  //this line grabs hold of the HTML doc and 
 		$('#racer2').css('margin-left', '+=5%');//if keycode ==32 increase the left margin 5%.
 
 	}
-	
-	if (event.which == keyup){
-		$('#racer2').css('margin-left', '==90%');
+
+
+/*$(document).ready(function(){
+	setInterval(function(){
+		if ($("#racer1").css('margin-left')=='90%'){
+			alert("Racer1 WINS!");
+		}
+	}
+
+});*/
+
+	if (event.which == 'margin-left'){ 
+		$('#racer2').css('margin-left', '==90%'){
 		alert("RACER 2 WINS!");
+		}
 	}
 
 	
 
-});//this is the end of the key up for 32 function
+});//end of the key up for 32 function
 
 
 
 
-});//this is the end of the document.ready(function) line
+});//end of the document.ready(function) line
