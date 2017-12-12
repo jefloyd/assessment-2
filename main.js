e10b786a5de88360ae76f4719c6cbcc88bbd2a23
John Edwin Floyd <johnedwinfloyd@Johns-MacBook.local>
2017-12-11 17:46:23 -0700

    winning conditional

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ $( document ).ready(function() {
 var bike1 = $(document).keyup(function(event) {//these two function are the same. One for racer1 key 29 and racer2 key 32.
 	console.log('hi3');
 	if (event.which == 39) {
-		$('#racer1').css('margin-left', '+=5%')
-		if($('#racer1').css('margin-left') === '90%')
+		$('#racer1').css('margin-left', '+=2%')
+		if($('#racer1').css('margin-left') >= '98%'){
 		alert('Racer 1 WINS!');
 	}
-
+}
 
 });//end of the keyup for 39 function 
 
@@ -20,12 +20,12 @@ var bike1 = $(document).keyup(function(event) {//these two function are the same
 //keycode 90 = Z button (Green motorbike)
 var bike2 = $(document).keyup(function(event) {  //this line grabs hold of the HTML doc and tells it to listen for a keyup even 
 	if (event.which == 90) { //this line says that if the keyup function is keycode 90 to perform line below
-		$('#racer2').css('margin-left', '+=5%')//if keycode ==32 increase the left margin 5%.
-		if($('#racer2').css('margin-left') === '90%')
+		$('#racer2').css('margin-left', '+=2%')//if keycode ==32 increase the left margin 5%.
+		if($('#racer2').css('margin-left') >= '98%'){
 		alert('Racer 2 WINS!');
 	} 
 	
-
+}
 
 });//end of the key up for 90 function
 
